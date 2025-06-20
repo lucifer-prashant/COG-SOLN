9accf8e4046698db074657d618b6b636f789dbe0
Prashant <163447777+lucifer-prashant@users.noreply.github.com>
2025-06-20 15:55:31 +0530

    wiped repo to reset structure and java files

--- WEEK1/MANDATORY/SingletonPatternExample/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Logger logger1 = Logger.getInstance();
-        Logger logger2 = Logger.getInstance();
-        logger1.log("FIRST MSG");
-        logger2.log("SECOND MSG");
-        System.out.println("ARE LOGGER 1 AND 2 THE SAME? "+(logger1==logger2));
-    }
-}
